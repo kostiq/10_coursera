d1476b6e827e203275a225ab3fea0fc7c1f53196
k.v.kuzmin <k.v.kuzmin@tinkoff.ru>
2017-02-17 12:15:03 +0300

    remove hidden dependencies

--- coursera.py
+++ coursera.py
@@ -1,6 +1,5 @@
 import requests
 import random
-from collections import OrderedDict
 import re
 from lxml import etree
 from bs4 import BeautifulSoup
@@ -34,7 +33,7 @@ def get_duration(html, soup):
     return duration
 
 
-def get_avr_star(html, soup):
+def get_avg_star(html, soup):
     if soup.find('div', {'class': re.compile('ratings-text')}):
         return soup.find('div', {'class': re.compile('ratings-text')}).text
 
@@ -47,21 +46,27 @@ def get_course_info(course_slug):
     course_info = []
     html = requests.get(course_slug).content
     soup = BeautifulSoup(html, 'html.parser')
-    course_info = {1: ('link', course_slug),
-                   2: ('title', get_title(html, soup)),
-                   3: ('start_date', get_start_date(html, soup)),
-                   4: ('duration', get_duration(html, soup)),
-                   5: ('language', get_language(html, soup)),
-                   6: ('avr_star', get_avr_star(html, soup))
+    course_info = {'link': course_slug,
+                   'title': get_title(html, soup),
+                   'start_date': get_start_date(html, soup),
+                   'duration': get_duration(html, soup),
+                   'language': get_language(html, soup),
+                   'avg_star': get_avg_star(html, soup)
                    }
 
     return course_info
 
 
 def write_to_sheet(sheet, courses_info):
+    ordered_course_fields = (
+        'link', 'title', 'start_date', 'duration', 'language', 'avg_star')
     for x, course_info in enumerate(courses_info, start=1):
         for y, info in enumerate(course_info, start=1):
-            sheet.cell(row=x, column=y, value=course_info[y][1])
+            sheet.cell(
+                row=x,
+                column=y,
+                value=course_info[str(ordered_course_fields[y - 1])]
+            )
 
 
 def output_courses_info_to_xlsx(filepath, courses_info):
@@ -73,4 +78,6 @@ def output_courses_info_to_xlsx(filepath, courses_info):
 
 if __name__ == '__main__':
     output_courses_info_to_xlsx(
-        'info.xlsx', [get_course_info(link) for link in get_list_of_random_courses()])
+        'info.xlsx',
+        [get_course_info(link) for link in get_list_of_random_courses()]
+    )
